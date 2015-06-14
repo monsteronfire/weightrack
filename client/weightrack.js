8d5e997f8ba8748f17cfe48f4d3e39562d0b74bf
Shaii Ong <shaii@Shaiis-iMac.local>
2015-06-14 14:43:25 +0800

    adds Facebook login

--- client/weightrack.js
+++ client/weightrack.js
@@ -1,23 +1,25 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    Template.login.events({
+        'click #facebook-login': function(event){
+            Meteor.loginWithFacebook({}, function(err){
+                if(err){
+                    throw new Meteor.Error('Facebook login failed');
+                }
+            });
+        },
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+        'click #logout': function(event){
+            Meteor.logout(function(err){
+                if(err){
+                    throw new Meteor.Error('Logout failed');
+                }
+            });
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+    Meteor.startup(function () {
     // code to run on server at startup
-  });
+    });
 }
---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+	service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+	service : 'facebook',
+	appID   : '381020622103087',
+	secret  : '290ae43f3a7c12852c510b73eee1419d'
+});
